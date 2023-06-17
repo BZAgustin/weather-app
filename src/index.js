794dbb962989338ae89a329c357140bf4dd5e92e
BZAgustin <abustoszanelli@gmail.com>
2023-06-17 15:04:40 -0300

    Fix fetch to use HTTPS

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const icons = iconManager();
 
 async function getLocationWeather(location) {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=223244cbdba14658867150217233105&q=${location}`, {mode: 'cors'});
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=223244cbdba14658867150217233105&q=${location}`, {mode: 'cors'});
     const weather = await response.json();
 
     return weather;
